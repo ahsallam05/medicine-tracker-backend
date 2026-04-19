0c1f0653e89daec220f1ec539f0711f27c5d69ec
ahmed <ahsallam06@gmail.com>
2026-04-20 01:13:26 +0200

    Updated CORS

--- src/app.js
+++ src/app.js
@@ -27,7 +27,17 @@ const limiter = rateLimit({
 });
 
 app.use(helmet());
-app.use(cors());
+
+const corsOptions = {
+  origin: [
+    'http://localhost:5173',
+    'https://my-medicine-tracker.netlify.app'
+  ],
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(limiter);
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
